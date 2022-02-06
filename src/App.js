42715ec0d9d8e8f6dc4d93a8f3e48da2071fb7c7
fumiyamiyazaki <57476802+fumiyamiyazaki@users.noreply.github.com>
2022-02-06 16:43:08 +0900

    controlle-rendering

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
+import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
 export default function App() {
   console.log("app");
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
 
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  };
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = (e) => setOpen(!open);
+
+  const onClickClose = useCallback(() => setOpen(false), []);
+
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <p>{count}</p>
-      <button onClick={onClickCountUp}>カウントアップ</button>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
